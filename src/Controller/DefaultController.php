301d7c41fc0b0b2fc2b0232697b4cf43f5d551f0
Oskar KWiatek <6005526@mborijnland.nl>
2021-10-11 13:40:29 +0200

    update

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -7,19 +7,22 @@ use App\Form\SearchType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'default')]
-    public function index(Request $request): Response
+    public function index(Request $request,UserPasswordHasherInterface $userPasswordHasherInterface): Response
     {
         $form = $this->createForm(SearchType::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            //var_dump($form->getViewData()["search"]);
+            //return $this->render("default/index.html.twig");
+            'test_entities' => $testEntityRepository-> findBy(["body"])
 
-            return $this->render("default/index.html.twig");
         }
 
         return $this->render('default/index.html.twig', [
