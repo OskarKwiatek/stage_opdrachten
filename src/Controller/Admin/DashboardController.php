e94ba5f66cf7c77b3b8f758b355d2bb37fa3bb17
Oskar KWiatek <6005526@mborijnland.nl>
2021-10-11 06:42:16 +0200

    easyadmin menu

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller\Admin;
 
+use App\Entity\TestEntity;
+use App\Entity\User;
 use EasyCorp\Bundle\EasyAdminBundle\Config\Dashboard;
 use EasyCorp\Bundle\EasyAdminBundle\Config\MenuItem;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractDashboardController;
@@ -27,6 +29,8 @@ class DashboardController extends AbstractDashboardController
     public function configureMenuItems(): iterable
     {
         yield MenuItem::linktoDashboard('Dashboard', 'fa fa-home');
-        // yield MenuItem::linkToCrud('The Label', 'fas fa-list', EntityClass::class);
+        yield MenuItem::linktoDashboard('Home', 'fa fa-backward',"default");
+        yield MenuItem::linkToCrud('User', 'fas fa-list', User::class);
+        yield MenuItem::linkToCrud('TestEntity', 'fas fa-list', TestEntity::class);
     }
 }
