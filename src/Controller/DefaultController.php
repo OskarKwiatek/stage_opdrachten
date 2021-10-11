18bca0e73d044727725e22b4e736b69952728c9b
Oskar KWiatek <6005526@mborijnland.nl>
2021-10-11 07:34:30 +0200

    search bar progress

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,17 +2,29 @@
 
 namespace App\Controller;
 
+use App\Form\RegistrationFormType;
+use App\Form\SearchType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends AbstractController
 {
     #[Route('/', name: 'default')]
-    public function index(): Response
+    public function index(Request $request): Response
     {
+        $form = $this->createForm(SearchType::class);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            return $this->render("default/index.html.twig");
+        }
+
         return $this->render('default/index.html.twig', [
             'controller_name' => 'DefaultController',
+            "sform" => $form -> createView(),
         ]);
     }
 }
